59dc52672df3aa41b4a893f1d1f9dce2ac758034
Julia-Hertseva <gerceva@gmail.com>
2022-10-30 14:49:46 +0200

    update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const renderGallery = galleryItems.reduce((acc, { preview, original, description }) => {
+    return acc + `<a class="gallery__item" href="${original}">
+    <img class="gallery__image"
+      src="${preview}"
+      alt="${description}"/>
+  </a>`}, "");
+
+console.log(renderGallery);
+
+const picture = document.querySelector(`.gallery`);
+picture.insertAdjacentHTML('beforeend', renderGallery)
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "bottom"
+});
